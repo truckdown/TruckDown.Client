c614906422d5a23f57504ef8b894eab2c2d7f79e
Peter Gordon <peterlgordon@msn.com>
2026-01-30 14:34:41 -0600

    Added abstractions project

---
+++ TruckDown.Abstractions/Authentication/AddresValidationResult.cs
@@ -0,0 +1,18 @@
+﻿namespace TruckDown.Client.Authentication;
+
+public class AddresValidationResult
+{
+    [JsonPropertyName("address")]
+    public string Address { get; set; } = string.Empty;
+
+    [MemberNotNullWhen(true, nameof(Expires))]
+    [MemberNotNullWhen(true, nameof(ValidationToken))]
+    [JsonPropertyName("isValid")]
+    public bool IsValid { get; set; }
+
+    [JsonPropertyName("expires")]
+    public DateTime? Expires { get; set; }
+
+    [JsonPropertyName("validationToken")]
+    public string? ValidationToken { get; set; }
+}
---
+++ TruckDown.Abstractions/Authentication/ContextInformation.cs
@@ -0,0 +1,31 @@
+﻿namespace TruckDown.Client.Authentication;
+
+public class ContextInformation
+{
+    [MemberNotNullWhen(true, nameof(Id))]
+    [MemberNotNullWhen(true, nameof(AccountId))]
+    [MemberNotNullWhen(true, nameof(AccountNumber))]
+    [MemberNotNullWhen(true, nameof(Username))]
+    [JsonPropertyName("isAuthenticated")]
+    public bool IsAuthenticated { get; set; }
+
+    [JsonPropertyName("id")]
+    public string? Id { get; set; }
+
+    [JsonPropertyName("accountId")]
+    public string? AccountId { get; set; }
+
+    [JsonPropertyName("accountNumber")]
+    public int? AccountNumber { get; set; }
+
+    [JsonPropertyName("username")]
+    public string? Username { get; set; }
+
+    [JsonPropertyName("settings")]
+    public Dictionary<string, string> Settings { get; set; }
+        = [];
+
+    [JsonPropertyName("claims")]
+    public string[] Claims { get; set; }
+        = [];
+}
